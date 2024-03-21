c8baba55f91b909ce7da0696969da845c9f97fc6
Bhuvan Singla <bhuvansingla2000@gmail.com>
2024-03-21 19:42:58 +0530

    Add workspace management example

---
+++ examples/workspaces/management.py
@@ -0,0 +1,8 @@
+# First party modules
+from paradime import Paradime
+
+# Create a Paradime client with your API credentials
+paradime = Paradime(api_endpoint="API_ENDPOINT", api_key="API_KEY", api_secret="API_SECRET")
+
+# Get all workspaces
+workspaces = paradime.workspaces.list_all()
