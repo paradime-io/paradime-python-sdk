df2b1e348098ebe961841b6a6b5f0c4edacdff2f
Kaustav Mitra <kaustav@paradime.io>
2024-04-06 19:27:02 +0100

    formatted error output for bolt

--- paradime/cli/version.py
+++ paradime/cli/version.py
@@ -1,6 +1,13 @@
 import click
+from rich import box
+from rich.console import Console
+from rich.text import Text
+from rich.json import JSON
+from rich.panel import Panel
+from rich.table import Table
 
 from paradime.version import get_sdk_version
+from rich_text_output import print_cli_header
 
 
 @click.command()
@@ -8,4 +15,7 @@ def version() -> None:
     """
     Get the version of the Paradime CLI.
     """
-    click.echo(get_sdk_version())
+    print_cli_header(get_sdk_version())
+
+if __name__ == '__main__':
+    version()
