d02142586a68e104e2b167cf9d7ab1db1a60f78e
Kaustav Mitra <kaustav@paradime.io>
2024-04-06 19:26:36 +0100

    rich text output for error and cli version

---
+++ paradime/cli/rich_text_output.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+from rich import box
+from rich.console import Console
+from rich.text import Text
+from rich.json import JSON
+from rich.panel import Panel
+from rich.table import Table
+
+console = Console()
+
+def print_cli_header(version: str) -> None:
+    version_title = Text(f'Paradime CLI ', style='green').append(f'v{version}', style='bold green')
+    version_title.append(f'\n\nUse the Paradime CLI to login and trigger Bolt dbt schedules from the terminal.', style='#9696a0')
+    version_title.append(f'\n\nRead examples of using the CLI and the SDK: ', style='#9696a0')
+    version_title.append(f'https://github.com/paradime-io/paradime-python-sdk/tree/main/examples', style='underline #9696a0')
+    console.print(Panel(version_title, padding=(1,2), width=100), style='#827be6')
+
+def print_error_table(error: Any, json: bool) -> None:
+    table = Table(border_style='#787885', box=box.SIMPLE, show_footer=True, width=100)
+    table.add_column("🚨 Error", justify="left", style="red", no_wrap=False)
+    if json:
+        table.add_row(JSON.from_data(error, highlight=False), style='#f44336')
+    else:
+        table.add_row(Text(error), style='#f44336')
+    console.print(table)
+    
