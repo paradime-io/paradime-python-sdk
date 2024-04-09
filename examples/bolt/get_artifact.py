ac75f1296cd5e5b9260cf886796f7f4b96154169
Bhuvan Singla <bhuvansingla2000@gmail.com>
2024-04-09 17:06:33 +0530

    Update get_artifact example

--- examples/bolt/get_artifact.py
+++ examples/bolt/get_artifact.py
@@ -4,18 +4,13 @@ from paradime import Paradime
 # Create a Paradime client with your API credentials
 paradime = Paradime(api_endpoint="API_ENDPOINT", api_key="API_KEY", api_secret="API_SECRET")
 
-BOLT_SCHEDULE_RUN_ID = 1  # Replace with the run ID of the Bolt schedule to get the manifest for
+BOLT_SCHEDULE_NAME = "daily_run"
+
+# Get manifest.json dictionary.
+manifest_json = paradime.bolt.get_latest_manifest_json(schedule_name=BOLT_SCHEDULE_NAME)
 
 
-commands = paradime.bolt.list_run_commands(BOLT_SCHEDULE_RUN_ID)  # Get the commands for the run
-
-for command in commands:
-    # Get the artifacts for the command
-    artifacts = paradime.bolt.list_command_artifacts(command.id)
-
-    for artifact in artifacts:
-        # If the artifact is the manifest, get the URL and print it
-        if "manifest.json" in artifact.path:
-            manifest_url = paradime.bolt.get_artifact_url(artifact.id)
-            print(f"Manifest URL: {manifest_url}")
-            break
+# Get any artifact.
+artifact_url = paradime.bolt.get_latest_artifact_url(
+    schedule_name=BOLT_SCHEDULE_NAME, artifact_path="target/catalog.json"
+)
