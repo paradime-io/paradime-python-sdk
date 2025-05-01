54583e4abc584e304d4366f6b6d2d57c30e864e9
Bhuvan Singla <bhuvansingla2000@gmail.com>
2025-05-01 15:29:22 +0530

    Add catalog refresh support

---
+++ examples/catalog/catalog_refresh.py
@@ -0,0 +1,8 @@
+# First party modules
+from paradime import Paradime
+
+# Create a Paradime client with your API credentials
+paradime = Paradime(api_endpoint="API_ENDPOINT", api_key="API_KEY", api_secret="API_SECRET")
+
+# Trigger a background refresh of the Paradime catalog
+paradime.catalog.refresh()
